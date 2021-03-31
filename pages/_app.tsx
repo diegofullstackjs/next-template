e3dd03799ad7c8a8b9220925aed2ba8eec1cbf8c
kelvin diego <kelvin.fabiana@gmail.com>
2021-03-31 14:20:56 -0300

    Initial template

---
+++ pages/_app.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {GetServerSideProps} from 'next'
+import Head from 'next/head';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import theme from '../src/theme/theme';
+
+interface IPropsApp {
+  Component: React.FC,
+  pageProps: GetServerSideProps
+}
+const MyApp: React.FC<IPropsApp> = ({Component,pageProps}) => {
+
+  React.useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <React.Fragment>
+      <Head>
+        <title>Ecommerce</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </React.Fragment>
+  );
+}
+
+export default MyApp
+
